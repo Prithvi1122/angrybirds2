99037bfd89b04d70cf9848504cfd89cbafaa0a63
Prithvi1122 <65720342+Prithvi1122@users.noreply.github.com>
2020-06-06 20:12:02 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,6 @@
+class Box extends BaseClass{
+    constructor(x,y,w,h){
+        super(x,y,w,h,0);
+        this.image=loadImage("sprites/wood1.png");
+    }
+}
---
+++ Log.js
@@ -0,0 +1,6 @@
+class Log extends BaseClass{
+    constructor(x,y,h,a){
+        super(x,y,20,h,a);
+        this.image=loadImage("sprites/wood2.png");
+    }
+}
